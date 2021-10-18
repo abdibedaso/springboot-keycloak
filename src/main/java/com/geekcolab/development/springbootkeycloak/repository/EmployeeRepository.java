a8e3ddd8abcbae120257b1db718e9488b13cd5a7
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-17 22:48:35 -0500

    extend JpaRepository to employee

--- src/main/java/com/geekcolab/development/springbootkeycloak/repository/EmployeeRepository.java
+++ src/main/java/com/geekcolab/development/springbootkeycloak/repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.geekcolab.development.springbootkeycloak.repository;
 
+import com.geekcolab.development.springbootkeycloak.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
